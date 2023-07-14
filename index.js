84722bb5fbcbc277c5e5fa2017f40cff3b2bef50
Romanovskyi2137 <113349151+Romanovskyi2137@users.noreply.github.com>
2023-07-14 14:34:49 +0300

    update

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const authRouter = require("./Routers/authRouter");
 const userRouter = require("./Routers/userRouter");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors({
+    origin: "*"
+}))
 app.use(express.json());
 app.use("/authorization", authRouter)
 app.use("/task_space", userRouter);
